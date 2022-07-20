45e43c9f5ce28fd39cad11bd1585f16166cc9e2e
ZDric2 <zedric.buiza@g.batstate-u.edu.ph>
2022-07-20 23:30:00 +0800

    pangalawa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    { title: 'Homepage', url: '/folder/home', icon: 'home' },
     { title: 'About Me', url: '/i', icon: 'person' },
     { title: 'Education', url: '/am', icon: 'book' },
     { title: 'Favorite Kpop G-group', url: '/zed', icon: 'heart' },
     { title: 'Contact', url: '/h', icon: 'link' },
+    
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
